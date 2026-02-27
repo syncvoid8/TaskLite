c5ed59da3ef54de8e18352ac609ca094c35c82d3
syncvoid8 <kazazayeva.olga@bk.ru>
2026-02-27 16:42:51 +0300

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+FILE = "tasks.json"
+
+def load_tasks():
+    if not os.path.exists(FILE):
+        return []
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+def save_tasks(tasks):
+    with open(FILE, "w") as f:
+        json.dump(tasks, f, indent=2)
